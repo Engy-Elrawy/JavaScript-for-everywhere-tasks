f9f72c7e77da403ebcb4561c91ebc75d5c32e093
Engy-Elrawy <engyelrawy5821@gmail.com>
2026-09-25 10:36:34 +0300

    predictions.md + predictions.js

---
+++ Day-02/predirections.js
@@ -0,0 +1,15 @@
+console.log('5'+3);
+console.log('5'-3);
+console.log(5+true);
+console.log('5'===5);
+console.log('5'==5);
+console.log(typeof null);
+console.log(typeof []);
+console.log(0||'defult'); 
+console.log(0??'defult');
+console.log(Boolean(''));
+console.log(Boolean('false'));
+console.log(Boolean([]));
+console.log(10%3);
+console.log(null+1);
+console.log(undefined+1); //undefined is not value 
